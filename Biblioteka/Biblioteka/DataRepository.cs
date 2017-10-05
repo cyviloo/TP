fa5a851992408183ff0521904724c3e6f5b15448
Borys Cywinski <cyviloo@gmail.com>
2017-10-05 22:04:53 +0200

    Add further functions for DataRepository

--- Biblioteka/Biblioteka/DataRepository.cs
+++ Biblioteka/Biblioteka/DataRepository.cs
@@ -13,6 +13,60 @@ namespace Biblioteka
             db = new DataContext();
         }
 
+        public void addKatalog(Katalog pozycja)
+        {
+            db.katalog.Add(System.Guid.NewGuid().ToString(), pozycja);
+        }
+
+        public Katalog getKatalog(String ID)
+        {
+            return db.katalog[ID];
+        }
+
+        public IEnumerable<Katalog> getAllKatalog()
+        {
+            return db.katalog.Values;
+        }
+
+        public void updateKatalog(String ID, Katalog pozycja)
+        {
+            db.katalog[ID] = pozycja;
+        }
+
+        public void deleteKatalog(String ID)
+        {
+            db.katalog.Remove(ID);
+        }
+
+        public void deleteKatalog(Katalog pozycja)
+        {
+            foreach (var item in db.katalog)
+                if (item.Value.Equals(pozycja))
+                    db.katalog.Remove(item.Key);
+        }
+
+        public void addWykaz(Wykaz element)
+        {
+            db.wykaz.Add(element);
+        }
+
+        public void getWykaz(int ID)
+        {
+            db.wykaz.Find(x => x.id == ID);
+        }
+
+        public IEnumerable<Wykaz> getAllWykaz()
+        {
+            return db.wykaz;
+        }
+
+        public void updateWykaz(int ID, Wykaz element)
+        {
+            Wykaz result = db.wykaz.Find(x => x.id == ID);
+            result = element;
+        }
+
+
         private class DataContext
         {
             public List<Wykaz> wykaz;
